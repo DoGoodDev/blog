790233d450ab4e5c8a749edc707e5612bc810600
Jordan Bergin <jordan.j.bergin@protonmail.com>
2022-03-26 22:46:44 +0000

    Configured gradle git and added a test tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,19 @@ buildscript {
     }
 }
 
+
 plugins {
     @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.jbake)
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.git.publish)
+}
+
+gitPublish {
+    repoUri.set("git@github.com:YouDoGood/blog.git")
+    branch.set("gh-pages")
+    sign.set(false)
+    contents {
+        from(tasks.named("bake"))
+    }
 }
